107b494416e645444fd219ef2bf5080af0ea132f
honglujun <909408954@qq.com>
2018-12-20 10:24:15 +0800

    建了config项目和demo项目 添加注释 修改了README.md文件
    #win10

--- everything-demo/src/main/java/com/example/everything/demo/EverythingDemoApplication.java
+++ everything-demo/src/main/java/com/example/everything/demo/EverythingDemoApplication.java
@@ -3,6 +3,9 @@ package com.example.everything.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author win10
+ */
 @SpringBootApplication
 public class EverythingDemoApplication {
 
