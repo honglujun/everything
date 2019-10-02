ee20ce0d923c0993b751293c4f8678fda61a5c42
honglujun <909408954@qq.com>
2019-10-02 20:13:16 +0800

    新增es数据
    #win10

---
+++ everything-es/src/test/java/com/example/everything/elastricksearch/EverythingEsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.everything.elastricksearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EverythingEsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
--- everything-order/src/main/java/com/example/everything/order/repository/ArticleRepository.java
+++ everything-order/src/main/java/com/example/everything/order/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.example.everything.order.repository;
 import com.example.everything.order.model.domain.Article;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * 继承ElasticsearchRepository的赠删改查的接口
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
  * @author win10
  */
 @Component
-//@Repository
+@Repository
 public interface ArticleRepository extends ElasticsearchRepository<Article, Long> {
 
 }
