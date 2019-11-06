5fd77996266d814388f38ac3c2193c1c0e354759
honglujun <909408954@qq.com>
2019-11-06 17:08:00 +0800

    升级springboot2.0.6到2.1.5 新增gateway网关 #win10

---
+++ everything-gateway/src/main/java/com/example/everything/gateway/EverythingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.everything.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverythingGatewayApplication.class, args);
+    }
+
+}
---
+++ everything-gateway/src/main/java/com/example/everything/gateway/filter/LoginFilter.java
@@ -0,0 +1,29 @@
+package com.example.everything.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class LoginFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getQueryParams().getFirst("token");
+        if (token == null || token.isEmpty()) {
+            log.info("token 为空，无法进行访问.");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
